93ee74f7e32216b46a3d44726ee5eabb94955b30
adefamosaBYU <fam24002@byui.edu>
2026-02-04 13:34:21 +0000

    Server port adjusted

--- server.js
+++ server.js
@@ -60,12 +60,12 @@ app.use(async (err, req, res, next) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 const host = process.env.HOST
 
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, "0.0.0.0", () => {
-  console.log(`app listening on ${port}`)
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`)
 })
