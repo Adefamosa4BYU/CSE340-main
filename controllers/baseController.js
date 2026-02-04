cd5f46cee305331cfd9036656e08362c3b097f9c
adefamosaBYU <fam24002@byui.edu>
2026-02-04 17:22:49 +0000

    Server port adjusted

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -2,8 +2,14 @@ const utilities = require("../utilities/")
 const baseController = {}
 
 baseController.buildHome = async function(req, res){
-  const nav = await utilities.getNav()
-  res.render("index", {title: "Home", nav})
+  let nav =[]
+  try {
+    nav = await utilities.getNav()
+    res.render("index", {title: "Home", nav})
+
+  } catch (err) {
+    console.error("Nav load failed:", err.message);
+  }
 }
 
 module.exports = baseController
