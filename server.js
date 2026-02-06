1951a2fa6fe181ca2ce5899c0743d986bad8c98a
adefamosaBYU <fam24002@byui.edu>
2026-02-06 01:19:34 +0000

    Week 4 Task

--- server.js
+++ server.js
@@ -9,18 +9,51 @@ const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
+const bodyParser = require("body-parser")
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const accountRoute = require("./routes/accountRoute")
 const utilities = require("./utilities/")
 const errorController = require("./controllers/errorController")
 const http = require("http")
 const port = process.env.PORT || 3000
 
+// Adding Database and Session Storage
+const session = require("express-session")
+const pool = require('./database/')
+const flash = require("connect-flash")
+const messages = require("express-messages")
+
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates
@@ -34,6 +67,9 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 
 // Inventory routes
 app.use("/inv", inventoryRoute)
+
+// Account Route
+app.use("/account", accountRoute)
 
 app.get("/cause-error", utilities.handleErrors(errorController.triggerError))
 
